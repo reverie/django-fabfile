fb48b9131c62f44559487daa8b1180dc4cb13866
Andrew Badr <andrewbadr+git@gmail.com>
2011-07-06 18:24:01 -0700

    change project name to foobar

--- server/django/wsgi.py
+++ server/django/wsgi.py
@@ -1,7 +1,7 @@
 import os, sys, site
-sys.path.insert(0, '/project/fixjam/current')
+sys.path.insert(0, '/project/foobar/current')
 site.addsitedir('/envs/default/lib/python2.6/site-packages/')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'fixjam.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'foobar.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
