dc95f875fb928c78995df618b09876075ec390f7
Andrew Badr <andrewbadr+git@gmail.com>
2011-07-08 14:42:38 -0700

    fix virtualenv in wsgi.py

--- server/django/wsgi.py
+++ server/django/wsgi.py
@@ -1,6 +1,8 @@
 import os, sys, site
 sys.path.insert(0, '/project/foobar/current')
-site.addsitedir('/envs/default/lib/python2.6/site-packages/')
+# The project root should be on the pythonpath. This lets you drop-in 3rd-party apps.
+sys.path.insert(0, '/project/foobar/current/foobar')
+site.addsitedir('/envs/foobar/lib/python2.6/site-packages/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'foobar.settings'
 
 import django.core.handlers.wsgi
