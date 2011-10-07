3a1b89d092b66fb659a50bd4ed8c67a8f636ed9f
Andrew Badr <andrewbadr+git@gmail.com>
2011-10-07 11:37:19 -0700

    fix static files

--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
 
+from common.views import req_render_to_response
+
 def index(request):
-    return render_to_response('index.html')
+    return req_render_to_response(request, 'index.html')
